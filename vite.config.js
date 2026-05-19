4bd8c637c1320ab69fe7d0796a3277975762ac76
Dhia Romdhane <dhia.romdhane@systaro.de>
2026-05-19 20:34:37 +0200

    fix: include all HTML pages in Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main:       resolve(__dirname, 'index.html'),
+        experience: resolve(__dirname, 'experience.html'),
+        projects:   resolve(__dirname, 'projects.html'),
+        skills:     resolve(__dirname, 'skills.html'),
+      }
+    }
+  }
 })
